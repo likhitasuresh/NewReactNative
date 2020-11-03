7153c005dbd18996e6489b688a10792c9e034df0
24/7papa <leshchevia@gmail.com>
2020-11-03 12:58:42 -0800

    hotfix

--- Components/NewChat.js
+++ Components/NewChat.js
@@ -46,12 +46,14 @@ class NewChat extends Component{
     for(let i = 0;i<messages.length;i++){
       //TODO: reset index once udate event pushed successfully
       messages[i].index = this.state.messages[0].index+1;
-      this.sendMessageFunction(this.chatInfo.channelSID,messages[i].text,messages[i].index);
-      //TODO: listen for the event of message delivered etc.
-      this.state.messages.unshift(messages[i]);
-      this.setState({
+      if(this.state.messages.indexOf(messages[i].index) === -1 ){
+        this.sendMessageFunction(this.chatInfo.channelSID,messages[i].text,messages[i].index);
+        //TODO: listen for the event of message delivered etc.
+        this.state.messages.unshift(messages[i]);
+        this.setState({
           messages: this.state.messages
         });
+      }
       }
     }
 
